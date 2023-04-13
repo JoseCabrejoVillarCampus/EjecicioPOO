2586db6f5aede3f5d8c5aa6e98568d83e5c9aa35
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-04-12 19:20:31 -0500

    feat: :sparkles: Creacion del metodo obtener y finalizacion del taller

--- main.js
+++ main.js
@@ -26,18 +26,52 @@ class lapiz {
         return this.material;
     }
     getMarca() {
-        return this.marca;
+        return this.#marca;
     }
+    obtener() {
+        const color = document.querySelector('[name="color"]').value;
+        const dimension = document.querySelector("#value").textContent;
+        const marca = document.querySelector('[name="marca"]:checked').value;
+        const borrador = document.querySelector('[name="borrador"]:checked').value === "true";
+        const material = document.querySelector('[name="material"]:checked').value;
+        return new lapiz({
+          color,
+          dimension,
+          marca,
+          borrador,
+          material,
+        });
+      }
 }
 let obj = undefined;
-let  color = document.querySelector(`[name="color]`)
-let  dimension = document.querySelector(`[name="value]`)
-let  borrador = document.querySelector(`[name="borrador]`)
-let  materialmarca = document.querySelector(`[name="materialmarca]`)
-let  marca = document.querySelector(`[name="marca]`)
+let  color = document.querySelector(`[name="color"]`)
+let  dimension = document.querySelector(`[id="value"]`)
+let  borrador = document.querySelector(`[name="borrador"]`)
+let  material = document.querySelector(`[name="material"]`)
+let  marca = document.querySelector(`[name="marca"]`)
 addEventListener("DOMContentLoaded",(e)=>{
-    obj = new lapiz();
+    obj = new lapiz({});
+    color.value = obj.color
+    dimension.value = obj.dimension
+    document.querySelector(`[name="marca"][value="${obj.marca}"]`).checked = true;
+    document.querySelector(`[name="borrador"][value="${obj.borrador}"]`).checked = true;
+    document.querySelector(`[name="material"][value="${obj.material}"]`).checked = true;
 })
+const tabla = document.querySelector("#tabla-lapices");
+const formulario = document.querySelector("#myForm");
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const Lapiz = new lapiz({}).obtener();
+  const row = tabla.insertRow();
+  row.insertCell().textContent = Lapiz.getColor();
+row.insertCell().textContent = Lapiz.getDimension();
+row.insertCell().textContent = Lapiz.getMarca();
+row.insertCell().textContent = Lapiz.getBorrador() ? "Sí" : "No";
+row.insertCell().textContent = Lapiz.getMaterial();
+  formulario.reset();
+});
+
 const value = document.querySelector("#value")
 const input = document.querySelector("#pi_input")
 value.textContent = input.value
